e5cf354d246b78c0595ffcbff5cb8a90da772af8
Deepak Kumar <mr.deepaknagvanshi@gmail.com>
2023-07-19 10:00:13 +0530

    done

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,6 +4,7 @@ import Herosection from '../components/HeroSection';
 function About(){
     return(
         <>
+          {/* it is hero section */}
           <Herosection title={"OUR STORY"} imageUrl={"/about1.svg"}/>
         </>
     )
